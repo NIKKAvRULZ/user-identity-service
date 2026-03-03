31f457fec225ae1e149ab8695e06b41c9c12998d
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2026-03-03 23:11:51 +0530

    Refactor UserRepository to extend JpaRepository and add custom query methods

--- src/main/java/com/foodsystem/user_identity_service/repository/UserRepository.java
+++ src/main/java/com/foodsystem/user_identity_service/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.foodsystem.user_identity_service.repository;
 
-public class UserRepository {
+import com.foodsystem.user_identity_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom query method to find a user by username
+    Optional<User> findByUsername(String username);
     
+    // Custom method to check if a user exists by email
+    boolean existsByEmail(String email);
 }
