e76225be48a9430480d05486c11b623e98d68a37
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2026-03-09 15:29:20 +0530

    Add endpoint to retrieve all registered users and implement corresponding service method

--- src/main/java/com/foodsystem/user_identity_service/service/UserService.java
+++ src/main/java/com/foodsystem/user_identity_service/service/UserService.java
@@ -66,4 +66,8 @@ public class UserService {
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
